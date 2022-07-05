aad8be3a15b6519f66c34a77d8fd5febb8b13c50
DavidHsu <davidhsu1115@gmail.com>
2022-07-05 20:53:55 +0800

    Use @Primary @Bean for java configuration

--- src/main/java/com/david/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/com/david/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,31 @@
 package com.david.sfgdi.config;
 
-import com.david.sfgdi.services.ConstructorGreetingService;
-import com.david.sfgdi.services.PropertyInjectedGreetingService;
-import com.david.sfgdi.services.SetterInjectedGreetingService;
+import com.david.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+    
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18nSpanishGreetingService i18nSpanishGreetingService(){
+        return new I18nSpanishGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nEnglishGreetingService(){
+        return new I18nEnglishGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService(){
